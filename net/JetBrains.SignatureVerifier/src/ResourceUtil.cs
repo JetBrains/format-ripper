25cc881963762f51443f676fd053223d17612564
Mikhail Pilin <mikhail@pilin.name>
2023-03-27 18:59:58 +0200

    Revert DefaultRoots.p7b resource

---
+++ net/JetBrains.SignatureVerifier/src/ResourceUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace JetBrains.SignatureVerifier
+{
+  public static class ResourceUtil
+  {
+    public static TResult OpenDefaultRoots<TResult>([NotNull] Func<Stream, TResult> handler) => OpenRead("DefaultRoots.p7b", handler);
+
+    private static TResult OpenRead<TResult>(string resourceName, Func<Stream, TResult> handler)
+    {
+      var type = typeof(ResourceUtil);
+      var fullResourceName = type.Namespace + ".Resources." + resourceName;
+      using var stream = type.Assembly.GetManifestResourceStream(fullResourceName);
+      if (stream == null)
+        throw new InvalidOperationException($"Failed to open resource stream for {fullResourceName}");
+      return handler(stream);
+    }
+  }
+}
---
+++ net/JetBrains.SignatureVerifier/tests/ResourceTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace JetBrains.SignatureVerifier.Tests
+{
+  [TestFixture]
+  public sealed class ResourceTest
+  {
+    [Test]
+    public void Test() => SignatureVerifier.ResourceUtil.OpenDefaultRoots(_ => false);
+  }
+}
