0c8778f7f0d2033ce2fea3f2021acb84c89ad3cc
ArtemKar123 <wartemtem@yandex.ru>
2023-06-27 12:52:46 +0200

    ByteArraySerializer

--- jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/Serializers.kt
+++ jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/Serializers.kt
@@ -1,9 +1,11 @@
 package com.jetbrains.signatureverifier.serialization
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.nullable
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.math.BigInteger
@@ -53,6 +55,21 @@ object BigIntegerSerializer : KSerializer<BigInteger> {
 
   override fun deserialize(decoder: Decoder): BigInteger = decoder.decodeString().toBigInteger()
 }
+
+object NullableByteArraySerializer : KSerializer<ByteArray?> {
+  @OptIn(ExperimentalSerializationApi::class)
+  override fun serialize(encoder: Encoder, value: ByteArray?) =
+    value?.let { encoder.encodeString(it.toHexString()) } ?: encoder.encodeNull()
+
+
+  @OptIn(ExperimentalSerializationApi::class)
+  override fun deserialize(decoder: Decoder): ByteArray? =
+    if (decoder.decodeNotNullMark()) decoder.decodeString().toByteArray() else decoder.decodeNull()
+  
+  override val descriptor: SerialDescriptor =
+    PrimitiveSerialDescriptor("NullableByteArray", PrimitiveKind.STRING).nullable
+}
+
 
 object ByteArraySerializer : KSerializer<ByteArray> {
   override fun serialize(encoder: Encoder, value: ByteArray) =
--- jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/UnknownTypeInfo.kt
+++ jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/UnknownTypeInfo.kt
@@ -5,10 +5,11 @@ import org.bouncycastle.asn1.ASN1Primitive
 
 @Serializable
 data class UnknownTypeInfo<T : ASN1Primitive>(
-  val content: String
+  @Serializable(ByteArraySerializer::class)
+  val content: ByteArray
 ) : EncodableInfo {
-  constructor(obj: T) : this(obj.encoded.toHexString())
+  constructor(obj: T) : this(obj.encoded)
 
-  override fun toPrimitive(): ASN1Primitive = ASN1Primitive.fromByteArray(content.toByteArray())
+  override fun toPrimitive(): ASN1Primitive = ASN1Primitive.fromByteArray(content)
 
 }
