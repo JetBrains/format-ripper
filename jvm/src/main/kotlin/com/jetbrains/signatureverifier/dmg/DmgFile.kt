67146f429312f41a31beb4f03614111cac355386
ArtemKar123 <wartemtem@yandex.ru>
2023-07-07 10:57:09 +0200

    Remove magic consts, add UDIFResourceFile structure

--- jvm/src/main/kotlin/com/jetbrains/signatureverifier/dmg/DmgFile.kt
+++ jvm/src/main/kotlin/com/jetbrains/signatureverifier/dmg/DmgFile.kt
@@ -11,26 +11,64 @@ import java.security.MessageDigest
 
 class DmgFile(@NotNull stream: SeekableByteChannel) {
   val UDIFResourceFileSize = 512
-  val numDataChecksumOffset = 84L
   private val stream: SeekableByteChannel
   private var signedData: ByteArray? = null
   private var cmsData: ByteArray? = null
   private val codeSignaturePointer: CodeSignaturePointer
+  private val codeSignaturePointerOffset = 296L // counted according to the structure described below
 
+  /**
+   * Parses UDIFResourceFile, then uses CodeSignatureOffset to extract all signature-related data.
+   *
+   * UDIFResourceFile structure:
+   *  Signature             udifSignature // magic 'koly', 4 bytes
+   * 	Version               uint32        // 4 (as of 2013)
+   * 	HeaderSize            uint32        // sizeof(this) =  512 (as of 2013)
+   * 	Flags                 udifResourceFileFlag // 4 bytes
+   * 	RunningDataForkOffset uint64
+   * 	DataForkOffset        uint64 // usually 0, beginning of file
+   * 	DataForkLength        uint64
+   * 	RsrcForkOffset        uint64 // resource fork offset and length
+   * 	RsrcForkLength        uint64
+   * 	SegmentNumber         uint32 // Usually 1, can be 0
+   * 	SegmentCount          uint32 // Usually 1, can be 0
+   * 	SegmentID             types.UUID // 128-bit
+   *
+   * 	DataChecksumType uint32
+   * 	DataChecksumSize uint32
+   * 	DataChecksum [32]uint32
+   *
+   * 	PlistOffset uint64 // Offset and length of the blkx plist.
+   * 	PlistLength uint64
+   *
+   * 	Reserved1 [64]byte
+   *
+   *  -->offset from the beginning of the structure is 296<--
+   * 	CodeSignatureOffset uint64
+   * 	CodeSignatureLength uint64
+   *
+   * 	Reserved2 [40]byte
+   *
+   * 	MasterChecksum UDIFChecksum
+   *
+   * 	ImageVariant uint32 // Unknown, commonly 1
+   * 	SectorCount  uint64
+   *
+   * 	Reserved3 uint32
+   * 	Reserved4 uint32
+   * 	Reserved5 uint32
+   */
   init {
     this.stream = stream
     val reader = BinaryReader(stream.Rewind())
 
-    stream.Jump(stream.size() - UDIFResourceFileSize)
+    val UDIFOffset = stream.size() - UDIFResourceFileSize
+
+    stream.Jump(UDIFOffset)
     if (reader.ReadUInt32Be() != 0x6b6f6c79u) // 'koly' signature
       error("Could not identify UDIFResourceFile")
 
-    stream.Seek(numDataChecksumOffset - 4, SeekOrigin.Current)
-    val numDataChecksum = reader.ReadUInt32Be().toInt()
-
-    // skip to reserved
-    stream.Seek(numDataChecksum * 4L + 16, SeekOrigin.Current)
-    stream.Seek(64, SeekOrigin.Current)
+    stream.Jump(UDIFOffset + codeSignaturePointerOffset)
 
     codeSignaturePointer = CodeSignaturePointer(
       reader.ReadUInt64Be().toLong(),
@@ -83,12 +121,13 @@ class DmgFile(@NotNull stream: SeekableByteChannel) {
     val reader = BinaryReader(stream.Rewind())
     // if signature is empty
     if (codeSignaturePointer.length == 0L) {
-      hash.update(reader.ReadBytes(stream.size().toInt() - 512)) // exclude UDIF block
+      hash.update(reader.ReadBytes(stream.size().toInt() - UDIFResourceFileSize)) // exclude UDIF block
     } else {
       // Data before codeSignature
       hash.update(reader.ReadBytes(codeSignaturePointer.offset.toInt()))
 
-      val dataBeforeUDIFLength = stream.size() - (codeSignaturePointer.offset + codeSignaturePointer.length) - 512
+      val dataBeforeUDIFLength =
+        stream.size() - (codeSignaturePointer.offset + codeSignaturePointer.length) - UDIFResourceFileSize
       // If something is left before UDIF block
       if (dataBeforeUDIFLength > 0) {
         stream.Jump(codeSignaturePointer.offset + codeSignaturePointer.length)
