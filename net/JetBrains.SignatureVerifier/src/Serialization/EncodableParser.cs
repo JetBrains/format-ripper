1a20f9068ca6b9f8f2356fd1c27b1e1e5e9ebda1
ArtemKar123 <wartemtem@yandex.ru>
2023-07-12 17:22:31 +0200

    Refactor EncodableParser with switch statement

--- net/JetBrains.SignatureVerifier/src/Serialization/EncodableParser.cs
+++ net/JetBrains.SignatureVerifier/src/Serialization/EncodableParser.cs
@@ -8,41 +8,39 @@ public static class EncodableParser
 {
   public static IEncodableInfo ToEncodableInfo(this Asn1Object source)
   {
-    if (source is Asn1TaggedObject taggedObject)
-    {
-      return new TaggedObjectInfo(
-        taggedObject.IsExplicit(),
-        taggedObject.TagNo,
-        taggedObject.GetObject().ToAsn1Object().ToEncodableInfo()
-      );
-    }
+    switch (source)
+       {
+           case Asn1TaggedObject taggedObject:
+               return new TaggedObjectInfo(
+                   taggedObject.IsExplicit(),
+                   taggedObject.TagNo,
+                   taggedObject.GetObject().ToAsn1Object().ToEncodableInfo()
+               );
 
-    if (source is Asn1Sequence sequence)
-    {
-      return new SequenceInfo(sequence
-        .ToArray()
-        .Select(item => item.ToAsn1Object().ToEncodableInfo())
-        .ToList()
-      );
-    }
+           case Asn1Sequence sequence:
+               return new SequenceInfo(sequence
+                   .ToArray()
+                   .Select(item => item.ToAsn1Object().ToEncodableInfo())
+                   .ToList()
+               );
 
-    if (source is Asn1Set set)
-    {
-      return new SetInfo(set
-        .ToArray()
-        .Select(item => item.ToAsn1Object().ToEncodableInfo())
-        .ToList()
-      );
-    }
+           case Asn1Set set:
+               return new SetInfo(set
+                   .ToArray()
+                   .Select(item => item.ToAsn1Object().ToEncodableInfo())
+                   .ToList()
+               );
 
-    try
-    {
-      return TextualInfo.GetInstance(source);
-    }
-    catch (Exception e)
-    {
-      Console.WriteLine(e);
-      throw;
-    }
+           default:
+               try
+               {
+                   return TextualInfo.GetInstance(source);
+               }
+               catch (Exception e)
+               {
+                   Console.WriteLine(e);
+                   throw;
+               }
+       }
   }
 }
