eca431394a3af75d627a058530636df3237a031c
ArtemKar123 <wartemtem@yandex.ru>
2023-07-13 16:43:13 +0200

    Add Sign with chain test to machO (disabled)

--- jvm/src/test/kotlin/com/jetbrains/signatureverifier/tests/MachoSignatureVerifierTests.kt
+++ jvm/src/test/kotlin/com/jetbrains/signatureverifier/tests/MachoSignatureVerifierTests.kt
@@ -5,11 +5,13 @@ import com.jetbrains.signatureverifier.crypt.SignedMessage
 import com.jetbrains.signatureverifier.crypt.SignedMessageVerifier
 import com.jetbrains.signatureverifier.crypt.VerifySignatureStatus
 import com.jetbrains.signatureverifier.macho.MachoArch
+import com.jetbrains.util.TestUtil
 import com.jetbrains.util.TestUtil.getTestDataFile
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -53,6 +55,37 @@ class MachoSignatureVerifierTests {
     }
   }
 
+  @ParameterizedTest
+  @Disabled
+  @MethodSource("VerifySignWithChainTestProvider")
+  fun VerifySignWithChainTest(
+    machOResourceName: String,
+    expectedResult: VerifySignatureStatus,
+    codesignRootCertStoreResourceName: String
+  ) {
+    TestUtil.getTestDataInputStream("mach-o", codesignRootCertStoreResourceName).use { codesignroots ->
+      val verificationParams = SignatureVerificationParams(
+        codesignroots, withRevocationCheck = false
+      )
+
+      val results = TestUtil.getTestByteChannel("mach-o", machOResourceName).use { machOFileStream ->
+        val machoFiles = MachoArch(machOFileStream).Extract()
+
+        machoFiles.map {
+          val signatureData = it.GetSignatureData()
+          val signedMessage = SignedMessage.CreateInstance(signatureData)
+
+          val signedMessageVerifier = SignedMessageVerifier(ConsoleLogger.Instance)
+          runBlocking { signedMessageVerifier.VerifySignatureAsync(signedMessage, verificationParams) }
+        }
+      }
+
+      results.forEach {
+        Assertions.assertEquals(expectedResult, it.Status)
+      }
+    }
+  }
+
 
   companion object {
     @JvmStatic
@@ -71,6 +104,17 @@ class MachoSignatureVerifierTests {
     fun VerifySignInvalidSignatureFormatTestProvider(): Stream<Arguments> {
       return Stream.of(
         Arguments.of("libSystem.Net.Security.Native.dylib")
+      )
+    }
+
+    @JvmStatic
+    fun VerifySignWithChainTestProvider(): Stream<Arguments> {
+      return Stream.of(
+        Arguments.of("JetBrains.Profiler.PdbServer", VerifySignatureStatus.Valid, "apple_root.p7b"),
+        Arguments.of("cat", VerifySignatureStatus.Valid, "apple_root.p7b"),
+        Arguments.of("env-wrapper.x64", VerifySignatureStatus.Valid, "apple_root.p7b"),
+        Arguments.of("libMonoSupportW.x64.dylib", VerifySignatureStatus.Valid, "apple_root.p7b"),
+        Arguments.of("libhostfxr.dylib", VerifySignatureStatus.Valid, "apple_root.p7b"),
       )
     }
   }
