f10e2d00ec73734792b09015e7594e6a33d4d47b
ArtemKar123 <wartemtem@yandex.ru>
2023-07-14 12:41:38 +0200

    Fix and simplify CMSAlgorithmProtectionAttributeInfo

--- jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/CMSAlgorithmProtectionAttributeInfo.kt
+++ jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/CMSAlgorithmProtectionAttributeInfo.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.signatureverifier.serialization
 
 import kotlinx.serialization.Serializable
+import org.bouncycastle.asn1.ASN1ObjectIdentifier
+import org.bouncycastle.asn1.ASN1Sequence
 import org.bouncycastle.asn1.DLSequence
 import org.bouncycastle.asn1.DLTaggedObject
 import org.bouncycastle.asn1.cms.Attribute
@@ -9,26 +11,13 @@ import org.bouncycastle.asn1.x509.AlgorithmIdentifier
 @Serializable
 data class CMSAlgorithmProtectionAttributeInfo(
   override val identifier: TextualInfo,
-  val content: List<List<EncodableInfo>>
+  val content: EncodableInfo
 ) : AttributeInfo() {
 
   constructor(attribute: Attribute) : this(
     TextualInfo.getInstance(attribute.attrType),
-    attribute.attrValues.map { sequence ->
-      (sequence as DLSequence).map {
-        when (it) {
-          is DLSequence -> AlgorithmInfo(AlgorithmIdentifier.getInstance(it))
-          is DLTaggedObject -> TaggedObjectInfo(
-            it.isExplicit,
-            it.tagNo,
-            AlgorithmInfo(AlgorithmIdentifier.getInstance(it.baseObject))
-          )
-
-          else -> throw IllegalArgumentException("Unexpected algorithm protection identifier type")
-        }
-      }
-    }
+    attribute.attrValues.toEncodableInfo()
   )
 
-  override fun getPrimitiveContent() = content.map { it.toPrimitiveList().toDLSequence() }.toDLSet()
+  override fun getPrimitiveContent() = content.toPrimitive()
 }
