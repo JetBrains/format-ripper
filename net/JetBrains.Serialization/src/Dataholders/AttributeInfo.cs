19d667b4a4f411c81115cb99dbcfb1bdd377516c
ArtemKar123 <wartemtem@yandex.ru>
2023-08-03 16:27:23 +0200

    Add SignatureTimeStampAttributeInfo class

--- net/JetBrains.Serialization/src/Dataholders/AttributeInfo.cs
+++ net/JetBrains.Serialization/src/Dataholders/AttributeInfo.cs
@@ -44,8 +44,8 @@ public abstract class AttributeInfo : IEncodableInfo
       //   return new V2CertificateAttributeInfo(attribute);
       // case "1.2.840.113549.1.9.16.2.12":
       //   return new PublicKeyInfrastructureAttributeInfo(attribute);
-      // case "1.2.840.113549.1.9.16.2.14":
-        // return new SignatureTimeStampAttributeInfo(attribute);
+      case "1.2.840.113549.1.9.16.2.14":
+        return new SignatureTimeStampAttributeInfo(attribute);
       case "1.3.6.1.4.1.311.2.4.1":
         return new MSSpcNestedSignatureInfo(attribute);
       default:
---
+++ net/JetBrains.Serialization/src/Dataholders/SignatureTimeStampAttributeInfo.cs
@@ -0,0 +1,25 @@
+using Org.BouncyCastle.Asn1;
+using Attribute = Org.BouncyCastle.Asn1.Cms.Attribute;
+
+namespace JetBrains.Serialization;
+
+public class SignatureTimeStampAttributeInfo : AttributeInfo
+{
+  public override TextualInfo Identifier { get; }
+  public List<RSASignedDataInfo> Content { get; }
+
+  public SignatureTimeStampAttributeInfo(TextualInfo identifier, List<RSASignedDataInfo> content)
+  {
+    Identifier = identifier;
+    Content = content;
+  }
+
+  public SignatureTimeStampAttributeInfo(Attribute attribute)
+    : this(TextualInfo.GetInstance(attribute.AttrType),
+      attribute.AttrValues.ToArray().Select(av => RSASignedDataInfo.GetInstance(av as DerSequence)).ToList())
+  {
+  }
+
+  public override Asn1Encodable GetPrimitiveContent() =>
+    Content.Cast<IEncodableInfo>().ToList().ToPrimitiveList().ToDerSet();
+}
