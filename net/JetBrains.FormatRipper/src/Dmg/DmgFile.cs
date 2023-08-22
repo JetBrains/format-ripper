a253f12f912e54b267ccf0a321f198dbf9cd8808
ArtemKar123 <wartemtem@yandex.ru>
2023-08-22 13:33:02 +0200

    Refactor

--- net/JetBrains.FormatRipper/src/Dmg/DmgFile.cs
+++ net/JetBrains.FormatRipper/src/Dmg/DmgFile.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Text;
 using JetBrains.FormatRipper.Impl;
 using JetBrains.FormatRipper.MachO.Impl;
 
@@ -8,16 +7,15 @@ namespace JetBrains.FormatRipper.Dmg
 {
   public sealed class DmgFile
   {
-    static byte[] epxectedSignature = new byte[] { 0x6b, 0x6f, 0x6c, 0x79 };
-    private UDIFResourceFile Header;
+    private static readonly byte[] ExpectedSignature = new byte[] { 0x6b, 0x6f, 0x6c, 0x79 }; // 'koly'
     public readonly SignatureData? SignatureData;
 
     public static unsafe bool Is(Stream stream)
     {
-      stream.Position = stream.Length - 512;
+      stream.Position = stream.Length - sizeof(UDIFResourceFile);
       UDIFResourceFile header;
       StreamUtil.ReadBytes(stream, (byte*)&header, sizeof(UDIFResourceFile));
-      return MemoryUtil.ArraysEqual(header.udifSignature, 4, epxectedSignature);
+      return MemoryUtil.ArraysEqual(header.udifSignature, 4, ExpectedSignature);
     }
 
     public static DmgFile Parse(Stream stream)
@@ -27,39 +25,42 @@ namespace JetBrains.FormatRipper.Dmg
 
     private unsafe DmgFile(Stream stream)
     {
-      stream.Position = stream.Length - 512;
+      stream.Position = stream.Length - sizeof(UDIFResourceFile);
       UDIFResourceFile headerBuffer;
       StreamUtil.ReadBytes(stream, (byte*)&headerBuffer, sizeof(UDIFResourceFile));
-      Header = headerBuffer;
+      var header = headerBuffer;
 
-      fixed (byte* p = Header.udifSignature)
-      {
-        if (!MemoryUtil.ArraysEqual(p, 4, epxectedSignature))
-          throw new FormatException("Invalid KOLY magic");
-      }
+      if (!MemoryUtil.ArraysEqual(header.udifSignature, ExpectedSignature.Length, ExpectedSignature))
+        throw new FormatException("Invalid KOLY magic");
 
       byte[]? codeDirectoryBlob = null;
       byte[]? cmsSignatureBlob = null;
-      if (MemoryUtil.GetBeU8(Header.CodeSignatureOffset) > 0)
+      if (MemoryUtil.GetBeU8(header.CodeSignatureOffset) > 0)
       {
-        stream.Position = (long)MemoryUtil.GetBeU8(Header.CodeSignatureOffset);
+        stream.Position = (long)MemoryUtil.GetBeU8(header.CodeSignatureOffset);
 
         CS_SuperBlob cssb;
         StreamUtil.ReadBytes(stream, (byte*)&cssb, sizeof(CS_SuperBlob));
+
         if ((CSMAGIC)MemoryUtil.GetBeU4(cssb.magic) != CSMAGIC.CSMAGIC_EMBEDDED_SIGNATURE)
           throw new FormatException("Invalid Mach-O code embedded signature magic");
+
         var csLength = MemoryUtil.GetBeU4(cssb.length);
+
         if (csLength < sizeof(CS_SuperBlob))
           throw new FormatException("Too small Mach-O code signature super blob");
 
         var csCount = MemoryUtil.GetBeU4(cssb.count);
+
         fixed (byte* scBuf = StreamUtil.ReadBytes(stream, checked((int)csLength - sizeof(CS_SuperBlob))))
         {
           for (var scPtr = scBuf; csCount-- > 0; scPtr += sizeof(CS_BlobIndex))
           {
             CS_BlobIndex csbi;
             MemoryUtil.CopyBytes(scPtr, (byte*)&csbi, sizeof(CS_BlobIndex));
+
             var csOffsetPtr = scBuf + MemoryUtil.GetBeU4(csbi.offset) - sizeof(CS_SuperBlob);
+
             switch (MemoryUtil.GetBeU4(csbi.type))
             {
               case CSSLOT.CSSLOT_CODEDIRECTORY:
