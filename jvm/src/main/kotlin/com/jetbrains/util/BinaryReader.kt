8a3ade86e1dee0a00ce417bd5f61baca175f0a83
Egor Malyshev <egor.malyshev@gmail.com>
2023-09-20 14:16:57 +0200

    Let's have a finer logging level

--- jvm/src/main/kotlin/com/jetbrains/util/BinaryReader.kt
+++ jvm/src/main/kotlin/com/jetbrains/util/BinaryReader.kt
@@ -23,6 +23,13 @@ class BinaryReader(private val channel: ByteChannel) {
     return buffer.int.toUInt()
   }
 
+  fun ReadUInt32AlwaysLE(): UInt {
+    fill(4)
+
+    if (ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN))
+    return buffer.int.toUInt()
+  }
+
   fun ReadInt64(): Long {
     fill(8)
     return buffer.long
