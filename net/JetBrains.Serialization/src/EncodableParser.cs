dbb0059140fe5d7fd30bf740a4cb501fd74d7350
ArtemKar123 <wartemtem@yandex.ru>
2023-10-16 10:41:23 +0200

    Remove Console.WriteLine()

--- net/JetBrains.Serialization/src/EncodableParser.cs
+++ net/JetBrains.Serialization/src/EncodableParser.cs
@@ -9,38 +9,30 @@ public static class EncodableParser
   public static IEncodableInfo ToEncodableInfo(this Asn1Object source)
   {
     switch (source)
-       {
-           case Asn1TaggedObject taggedObject:
-               return new TaggedObjectInfo(
-                   taggedObject.IsExplicit(),
-                   taggedObject.TagNo,
-                   taggedObject.GetObject().ToAsn1Object().ToEncodableInfo()
-               );
+    {
+      case Asn1TaggedObject taggedObject:
+        return new TaggedObjectInfo(
+          taggedObject.IsExplicit(),
+          taggedObject.TagNo,
+          taggedObject.GetObject().ToAsn1Object().ToEncodableInfo()
+        );
 
-           case Asn1Sequence sequence:
-               return new SequenceInfo(sequence
-                   .ToArray()
-                   .Select(item => item.ToAsn1Object().ToEncodableInfo())
-                   .ToList()
-               );
+      case Asn1Sequence sequence:
+        return new SequenceInfo(sequence
+          .ToArray()
+          .Select(item => item.ToAsn1Object().ToEncodableInfo())
+          .ToList()
+        );
 
-           case Asn1Set set:
-               return new SetInfo(set
-                   .ToArray()
-                   .Select(item => item.ToAsn1Object().ToEncodableInfo())
-                   .ToList()
-               );
+      case Asn1Set set:
+        return new SetInfo(set
+          .ToArray()
+          .Select(item => item.ToAsn1Object().ToEncodableInfo())
+          .ToList()
+        );
 
-           default:
-               try
-               {
-                   return TextualInfo.GetInstance(source);
-               }
-               catch (Exception e)
-               {
-                   Console.WriteLine(e);
-                   throw;
-               }
-       }
+      default:
+        return TextualInfo.GetInstance(source);
+    }
   }
 }
