98619f8c3ca17d8d8ebeb7dac41407bf75328608
ArtemKar123 <wartemtem@yandex.ru>
2023-11-09 14:59:28 +0100

    JVM serialization format exceptions

--- jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/Serializers.kt
+++ jvm/src/main/kotlin/com/jetbrains/signatureverifier/serialization/Serializers.kt
@@ -16,106 +16,125 @@ import org.bouncycastle.asn1.DLTaggedObject
 @OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = ASN1Primitive::class)
 object Asn1PrimitiveSerializer : KSerializer<ASN1Primitive> {
-    override val descriptor = ContextualSerializer(ASN1Primitive::class).descriptor
+  override val descriptor = ContextualSerializer(ASN1Primitive::class).descriptor
 
-    override fun deserialize(decoder: Decoder): ASN1Primitive {
-        val jsonInput = decoder as? JsonDecoder ?: throw SerializationException("Only JSON format is supported")
-        return when (val jsonElement = jsonInput.decodeJsonElement()) {
-            is JsonObject -> {
-                if ("explicit" in jsonElement && "tagNo" in jsonElement && "object" in jsonElement) {
-                    val explicit = jsonElement["explicit"]!!.jsonPrimitive.boolean
-                    val tagNo = jsonElement["tagNo"]!!.jsonPrimitive.int
-                    val obj = Json.decodeFromJsonElement(Asn1PrimitiveSerializer, jsonElement["object"]!!)
-                    DLTaggedObject(explicit, tagNo, obj)
-                } else {
-                    val elements = jsonElement.map { (_, json) ->
-                        Json.decodeFromJsonElement(Asn1PrimitiveSerializer, json)
-                    }
-                    elements.toDLSequence()
-                }
+  override fun deserialize(decoder: Decoder): ASN1Primitive {
+    val jsonInput = decoder as? JsonDecoder ?: throw SerializationException("Only JSON format is supported")
+    return when (val jsonElement = jsonInput.decodeJsonElement()) {
+      is JsonObject -> {
+        if ("explicit" in jsonElement && "tagNo" in jsonElement && "object" in jsonElement) {
+          if (jsonElement.keys.count() != 3) {
+            throw SerializationException("Incorrect number of fields for tagged object: ${jsonElement.keys.count()}, expected 3")
+          }
+          val explicit =
+            jsonElement["explicit"]?.jsonPrimitive?.boolean
+              ?: throw SerializationException("\"Explicit\" is missing for tagged object")
+          val tagNo = jsonElement["tagNo"]?.jsonPrimitive?.int
+            ?: throw SerializationException("\"TagNo\" is missing for tagged object")
+          val obj = Json.decodeFromJsonElement(
+            Asn1PrimitiveSerializer,
+            jsonElement["object"] ?: throw SerializationException("\"Object\" is missing for tagged object")
+          )
+          DLTaggedObject(explicit, tagNo, obj)
+        } else {
+          var c = 0
+          val elements = jsonElement.map { (i, json) ->
+            if (i.toIntOrNull() != c){
+              throw SerializationException("Illegal index for sequence: ${i.toIntOrNull()}, expected $c")
             }
-
-            is JsonArray -> {
-                val elements = jsonElement.map { json ->
-                    Json.decodeFromJsonElement(Asn1PrimitiveSerializer, json)
-                }
-                elements.toDLSet()
-            }
-
-            is JsonNull -> {
-                DERNull.INSTANCE
-            }
-
-            is JsonPrimitive -> {
-                if (jsonElement.isString) {
-                    val value = jsonElement.toString().substringAfter('"').substringBeforeLast('"')
-                    val tagEnd = value.indexOf(']')
-                    val tag = value.substring(1, tagEnd)
-
-                    val textualValue = value.substring(tagEnd + 2)
-
-                    TextualInfo.getPrimitive(tag, textualValue)
-                } else {
-
-                    return ASN1Boolean.getInstance(
-                        jsonElement.booleanOrNull ?: throw SerializationException("Unsupported ASN1Primitive type")
-                    )
-                }
-            }
-
-            else -> throw SerializationException("Unsupported JSON element type")
+            c++
+            Json.decodeFromJsonElement(Asn1PrimitiveSerializer, json)
+          }
+          elements.toDLSequence()
         }
-    }
+      }
 
-    @OptIn(ExperimentalSerializationApi::class)
-    override fun serialize(encoder: Encoder, value: ASN1Primitive) {
-        val jsonOutput = encoder as? JsonEncoder
-            ?: throw SerializationException("Only JSON format is supported")
-
-        when (value) {
-            is ASN1TaggedObject -> {
-                jsonOutput.encodeJsonElement(buildJsonObject {
-                    put("explicit", value.isExplicit)
-                    put("tagNo", value.tagNo)
-                    put("object", Json.encodeToJsonElement(Asn1PrimitiveSerializer, value.baseObject.toASN1Primitive()))
-                })
-            }
-
-            is ASN1Sequence -> {
-                jsonOutput.encodeJsonElement(buildJsonObject {
-                    value.forEachIndexed { index, item ->
-                        put(index.toString(), Json.encodeToJsonElement(Asn1PrimitiveSerializer, item.toASN1Primitive()))
-                    }
-                })
-            }
-
-            is ASN1Set -> {
-                jsonOutput.encodeJsonElement(buildJsonArray {
-                    value.forEach { item ->
-                        add(Json.encodeToJsonElement(Asn1PrimitiveSerializer, item.toASN1Primitive()))
-                    }
-                })
-            }
-
-            is ASN1Null -> {
-                jsonOutput.encodeJsonElement(JsonNull)
-            }
-
-            is ASN1Boolean -> {
-                jsonOutput.encodeJsonElement(JsonPrimitive(value.isTrue))
-            }
-
-            else -> {
-                jsonOutput.encodeJsonElement(
-                    JsonPrimitive(
-                        "[${TextualInfo.getType(value)}] ${
-                            TextualInfo.getStringValue(
-                                value
-                            )
-                        }"
-                    )
-                )
-            }
+      is JsonArray -> {
+        val elements = jsonElement.map { json ->
+          Json.decodeFromJsonElement(Asn1PrimitiveSerializer, json)
         }
+        elements.toDLSet()
+      }
+
+      is JsonNull -> {
+        DERNull.INSTANCE
+      }
+
+      is JsonPrimitive -> {
+        if (jsonElement.isString) {
+          val value = jsonElement.toString().substringAfter('"').substringBeforeLast('"')
+          val tagEnd = value.indexOf(']')
+
+          if (value[0] != '[' || tagEnd < 0){
+            throw SerializationException("Could not parse tag for enrty $jsonElement")
+          }
+
+          val tag = value.substring(1, tagEnd)
+
+          val textualValue = value.substring(tagEnd + 2)
+
+          TextualInfo.getPrimitive(tag, textualValue)
+        } else {
+
+          return ASN1Boolean.getInstance(
+            jsonElement.booleanOrNull ?: throw SerializationException("Unsupported ASN1Primitive type: $jsonElement")
+          )
+        }
+      }
+
+      else -> throw SerializationException("Unsupported JSON element type")
     }
+  }
+
+  @OptIn(ExperimentalSerializationApi::class)
+  override fun serialize(encoder: Encoder, value: ASN1Primitive) {
+    val jsonOutput = encoder as? JsonEncoder
+      ?: throw SerializationException("Only JSON format is supported")
+
+    when (value) {
+      is ASN1TaggedObject -> {
+        jsonOutput.encodeJsonElement(buildJsonObject {
+          put("explicit", value.isExplicit)
+          put("tagNo", value.tagNo)
+          put("object", Json.encodeToJsonElement(Asn1PrimitiveSerializer, value.baseObject.toASN1Primitive()))
+        })
+      }
+
+      is ASN1Sequence -> {
+        jsonOutput.encodeJsonElement(buildJsonObject {
+          value.forEachIndexed { index, item ->
+            put(index.toString(), Json.encodeToJsonElement(Asn1PrimitiveSerializer, item.toASN1Primitive()))
+          }
+        })
+      }
+
+      is ASN1Set -> {
+        jsonOutput.encodeJsonElement(buildJsonArray {
+          value.forEach { item ->
+            add(Json.encodeToJsonElement(Asn1PrimitiveSerializer, item.toASN1Primitive()))
+          }
+        })
+      }
+
+      is ASN1Null -> {
+        jsonOutput.encodeJsonElement(JsonNull)
+      }
+
+      is ASN1Boolean -> {
+        jsonOutput.encodeJsonElement(JsonPrimitive(value.isTrue))
+      }
+
+      else -> {
+        jsonOutput.encodeJsonElement(
+          JsonPrimitive(
+            "[${TextualInfo.getType(value)}] ${
+              TextualInfo.getStringValue(
+                value
+              )
+            }"
+          )
+        )
+      }
+    }
+  }
 }
