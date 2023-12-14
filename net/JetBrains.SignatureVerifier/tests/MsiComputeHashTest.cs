4cfdff5047c38553fd682309e445acb5684bf088
Konstantin Kretov <konstantin.kretov@jetbrains.com>
2023-12-14 18:13:21 +0100

    NP-1632 Added test for the case where a file has multiple signatures and there are both valid and invalid signatures. Fixed formatting.

--- net/JetBrains.SignatureVerifier/tests/MsiComputeHashTest.cs
+++ net/JetBrains.SignatureVerifier/tests/MsiComputeHashTest.cs
@@ -8,8 +8,8 @@ namespace JetBrains.SignatureVerifier.Tests
   public class MsiComputeHashTest
   {
     // @formatter:off
-    [TestCase("2dac4b.msi"           , "SHA1", "EB0D6F874462F8ACC60C07D0D7B5A9C847EE311A")]
-    [TestCase("2dac4b_not_signed.msi", "SHA1", "CBBE5C1017C8A65FFEB9219F465C949563A0E256")]
+    [TestCase("2dac4b.msi"            , "SHA1",   "EB0D6F874462F8ACC60C07D0D7B5A9C847EE311A")]
+    [TestCase("2dac4b_not_signed.msi" , "SHA1",   "CBBE5C1017C8A65FFEB9219F465C949563A0E256")]
     [TestCase("2dac4b_self_signed.msi", "SHA256", "A930749F40001E6CBE1656720E4951CD8C70843BD4E1326EDE2402392E952025")]
     // @formatter:on
     [Test]
