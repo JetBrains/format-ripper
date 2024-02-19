27be37e3e05c82100d214607794295cad94db33e
Konstantin Kretov <konstantin.kretov@jetbrains.com>
2024-02-19 17:31:22 +0100

    NP-1662 First implementation of the Mach-O signature transfer

---
+++ net/JetBrains.SignatureVerifier/tests/MachOSignatureTransferTests.cs
@@ -0,0 +1,58 @@
+using System.IO;
+using System.Threading.Tasks;
+using JetBrains.FormatRipper.MachO;
+using JetBrains.FormatRipper.Pe;
+using JetBrains.SignatureVerifier.Crypt;
+using NUnit.Framework;
+using Org.BouncyCastle.Utilities;
+
+namespace JetBrains.SignatureVerifier.Tests;
+
+public class MachOSignatureTransferTests
+{
+  [TestCase("TestCppApp1_signed", "TestCppApp1")]
+  [TestCase("TestCppApp1_signed", "TestCppApp1_signed_removed")]
+  [TestCase("TestCppApp2_adhoc_signed", "TestCppApp2_adhoc")]
+  [TestCase("TestApp_developer", "TestApp_adhoc")]
+  [TestCase("TestApp_developer", "TestApp_not_signed")]
+  public async Task SignatureShouldBeTransfered(string donor, string acceptor)
+  {
+    var file = ResourceUtil.OpenRead(ResourceCategory.MachO, donor, stream => MachOFile.Parse(stream, MachOFile.Mode.SignatureData | MachOFile.Mode.ComputeHashInfo));
+
+    Assert.NotNull(file.Signature);
+
+    using MemoryStream resultFileStream = new MemoryStream();
+
+    ResourceUtil.OpenRead(ResourceCategory.MachO, acceptor, stream =>
+    {
+      MachOSignatureInjector.InjectSignature(stream, resultFileStream, file.Signature);
+      return 0;
+    });
+
+    resultFileStream.Seek(0, SeekOrigin.Begin);
+    using (var f = File.OpenWrite("output"))
+      resultFileStream.WriteTo(f);
+
+    MachOFile acceptorFile = MachOFile.Parse(resultFileStream, MachOFile.Mode.SignatureData | MachOFile.Mode.ComputeHashInfo);
+
+    var verificationParams = new SignatureVerificationParams(null, null, false, false);
+
+    MachOSignatureVerifier signatureVerifier = new MachOSignatureVerifier(ConsoleLogger.Instance);
+
+    var result =  await signatureVerifier.VerifyAsync(acceptorFile, resultFileStream, verificationParams, FileIntegrityVerificationParams.Default);
+
+    Assert.AreEqual(VerifySignatureStatus.Valid, result.Status, "Signature verification failure");
+
+    byte[] signedFileArray = ResourceUtil.OpenRead(ResourceCategory.MachO, donor, stream =>
+    {
+      byte[] data = new byte[stream.Length];
+      stream.Read(data, 0, data.Length);
+      return data;
+    });
+
+    byte[] acceptorFileArray = resultFileStream.ToArray();
+
+    Assert.AreEqual(signedFileArray.Length, acceptorFileArray.Length, "Length equality failure");
+    Assert.True(Arrays.AreEqual(signedFileArray, acceptorFileArray), "Byte equality failure");
+  }
+}
