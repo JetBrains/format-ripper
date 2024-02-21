7d08bd19c29ea263229a5a37c03a05ef863cfb33
Konstantin Kretov <konstantin.kretov@jetbrains.com>
2024-02-21 18:52:16 +0100

    NP-1662 Fixed DMG file padding handling. Added tests for improper Mach-o signatures transfer.

--- net/JetBrains.FormatRipper/src/Dmg/DmgSignatureInjector.cs
+++ net/JetBrains.FormatRipper/src/Dmg/DmgSignatureInjector.cs
@@ -21,19 +21,12 @@ public class DmgSignatureInjector
 
     long usefullPayloadLength = existingSignatureLength == 0 ? sourceStream.Length - sizeof(UDIF) : existingSignatureOffset;
 
-    if (usefullPayloadLength > signature.SignatureOffset)
+    if (usefullPayloadLength != signature.SignatureOffset)
       throw new SignatureInjectionException($"Cannot transfer the signature. Expected file size: {signature.SignatureOffset + sizeof(UDIF)} bytes, bug got {sourceStream.Length} bytes");
 
     long bytesToCopy = Math.Min(usefullPayloadLength, signature.SignatureOffset);
 
     StreamUtil.CopyBytes(sourceStream, outputStream, bytesToCopy);
-
-    if (bytesToCopy < signature.SignatureOffset)
-    {
-      long paddingBytes = signature.SignatureOffset - bytesToCopy;
-      byte[] padding = new byte[paddingBytes];
-      outputStream.Write(padding, 0, padding.Length);
-    }
 
     outputStream.Write(signature.SignatureBlob, 0, signature.SignatureBlob.Length);
     udif.CodeSignatureLength = MemoryUtil.GetBeU8((ulong)signature.SignatureLength);
