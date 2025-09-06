3cb1d9acaa5e6dd5bc760f8f78d9eb54252c11d9
Mikhail.Pilin <mikhail@pilin.name>
2025-09-06 22:17:00 +0200

    Add 'DigiCert Trusted Root G4' into valid certificate root list
    (cherry picked from commit 66bf34fbb984e0beeeff938c973299388164d631)

--- net/JetBrains.SignatureVerifier/tests/DefaultRootsTest.cs
+++ net/JetBrains.SignatureVerifier/tests/DefaultRootsTest.cs
@@ -18,6 +18,7 @@ namespace JetBrains.SignatureVerifier.Tests
           // @formatter:off
           ("Apple Root CA"                            , new BigInteger("02"                              , 16)),
           ("Certum Trusted Network CA"                , new BigInteger("0444c0"                          , 16)),
+          ("DigiCert Trusted Root G4"                 , new BigInteger("059b1b579e8e2132e23907bda777755c", 16)),
           ("Entrust Root Certification Authority - G2", new BigInteger("4a538c28"                        , 16)),
           ("Go Daddy Root Certificate Authority - G2" , new BigInteger("00"                              , 16)),
           ("Microsoft Root Certificate Authority"     , new BigInteger("79ad16a14aa0a5ad4c7358f407132e65", 16)),
