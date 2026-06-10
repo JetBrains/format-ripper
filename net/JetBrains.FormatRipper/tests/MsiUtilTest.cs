bed63ab7a69c22dcf7899e658684301ab4b0759c
Mikhail Pilin <mikhail@pilin.name>
2026-06-10 12:18:39 +0200

    Revert "MsiUtilTest"
    This reverts commit 49c4e7011fe82ec66203ceb32bc9d706f7b248f2.

--- net/JetBrains.FormatRipper/tests/MsiUtilTest.cs
+++ net/JetBrains.FormatRipper/tests/MsiUtilTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using JetBrains.FormatRipper.Compound;
-using Newtonsoft.Json;
+﻿using JetBrains.FormatRipper.Compound;
 using NUnit.Framework;
 
 namespace JetBrains.FormatRipper.Tests
@@ -11,92 +6,23 @@ namespace JetBrains.FormatRipper.Tests
   [TestFixture]
   public sealed class MsiUtilTest
   {
-    // Local logger implementation for test output
-    private sealed class ConsoleLogger : ILogger
-    {
-      public static readonly ILogger Instance = new ConsoleLogger();
-      private ConsoleLogger() { }
-      public void Info(string str) => Console.WriteLine($"INFO: {str}");
-      public void Warning(string str) => Console.Error.WriteLine($"WARNING: {str}");
-      public void Error(string str) => Console.Error.WriteLine($"ERROR: {str}");
-      public void Trace(string str) => Console.Error.WriteLine($"TRACE: {str}");
-    }
-
-    public class TestCase
-    {
-      public string name { get; set; }
-      public string expectedName { get; set; }
-      public string description { get; set; }
-    }
-
-    private static readonly ILogger Logger = ConsoleLogger.Instance;
-
-    private static IEnumerable<TestCaseData> LoadMsiTestCases()
-    {
-      var testCases = LoadTestCases();
-
-      return testCases.Select(testCase =>
-        new TestCaseData(testCase)
-          .SetName($"MsiDecode_{testCase.expectedName.Replace(".", "_")}")
-          .SetDescription($"Test MSI stream name decoding for {testCase.description}")
-      );
-    }
-
-    private static List<TestCase> LoadTestCases()
-    {
-      var type = typeof(ResourceUtil);
-      var resourceName = $"{type.Namespace}.MsiUtilTestCases.json";
-
-      using var stream = type.Assembly.GetManifestResourceStream(resourceName);
-      if (stream == null)
-        throw new InvalidOperationException($"Failed to open resource stream for {resourceName}");
-
-      using var reader = new StreamReader(stream);
-      var json = reader.ReadToEnd();
-      return JsonConvert.DeserializeObject<List<TestCase>>(json);
-    }
-
-    [TestCaseSource(nameof(LoadMsiTestCases))]
+    // @formatter:off
+    [TestCase(DirectoryNames.DigitalSignatureName      , DirectoryNames.DigitalSignatureName)]
+    [TestCase(DirectoryNames.DocumentSummaryInformation, DirectoryNames.DocumentSummaryInformation)]
+    [TestCase(DirectoryNames.MsiDigitalSignatureExName , DirectoryNames.MsiDigitalSignatureExName)]
+    [TestCase(DirectoryNames.SummaryInformationName    , DirectoryNames.SummaryInformationName)]
+    [TestCase(DirectoryNames.䡀_ColumnsName             , "䡀_Columns")]
+    [TestCase(DirectoryNames.䡀_StringDataName          , "䡀_StringData")]
+    [TestCase(DirectoryNames.䡀_StringPoolName          , "䡀_StringPool")]
+    [TestCase(DirectoryNames.䡀_TablesName              , "䡀_Tables")]
+    [TestCase(DirectoryNames.䡀_ValidationName          , "䡀_Validation")]
+    [TestCase("䡀䑒䗶䏤㾯㼒䔨䈸䆱䠨"                             , "䡀InstallUISequence")]
+    [TestCase("䡀䈏䗤䕸㬨䐲䒳䈱䗱䠶"                             , "䡀FeatureComponents")]
+    [TestCase("䌋䄱䜵䆾䖸䌷䒦䠱"                               , "Binary.custicon")]
+    [TestCase("䌋䄱䜵䅾䑤䕱䐥䠳"                               , "Binary.bannrbmp")]
+    [TestCase("䅧䞪䄦䠥"                                   , "dbg.cab")]
+    // @formatter:on
     [Test]
-    public void TestMsiDecodeStreamName(TestCase testCase)
-    {
-      Logger.Info($"Testing MSI stream name decoding: {testCase.description}");
-
-      if (testCase.name.StartsWith("DirectoryNames."))
-      {
-        var directoryName = testCase.name.Replace("DirectoryNames.", "");
-        var fieldInfo = typeof(DirectoryNames).GetField(directoryName);
-        if (fieldInfo != null)
-        {
-          var value = fieldInfo.GetValue(null);
-          if (value != null)
-          {
-            testCase.name = value as string;
-          }
-        }
-      }
-
-      if (testCase.expectedName.StartsWith("DirectoryNames."))
-      {
-        var directoryName = testCase.expectedName.Replace("DirectoryNames.", "");
-        var fieldInfo = typeof(DirectoryNames).GetField(directoryName);
-        if (fieldInfo != null)
-        {
-          var value = fieldInfo.GetValue(null);
-          if (value != null)
-          {
-            testCase.expectedName = value as string;
-          }
-        }
-      }
-
-
-      string decodedName = MsiUtil.MsiDecodeStreamName(testCase.name);
-
-      Assert.AreEqual(testCase.expectedName, decodedName,
-        $"Failed to decode MSI stream name '{testCase.name}' correctly");
-
-      Logger.Info($"Successfully decoded '{testCase.name}' to '{decodedName}'");
-    }
+    public void Test(string name, string expectedName) => Assert.AreEqual(expectedName, MsiUtil.MsiDecodeStreamName(name));
   }
 }
